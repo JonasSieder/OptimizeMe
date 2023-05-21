65e27c9681e96a946d4b0f97128b638a57c75b00
Jonas Sieder <j.sieder@stroeermediabrands.de>
2023-05-21 06:57:48 +0200

    nice perfect working pomodoro v2

--- src/js/components/pomodoro-progressbar.js
+++ src/js/components/pomodoro-progressbar.js
@@ -1,6 +1,6 @@
 const init = async () => {
   const intervalTimes = await getIntervalTime()
-  renderProgressbar(intervalTimes.workInterval, intervalTimes.breakInterval, intervalTimes.lastBreak, intervalTimes.pomodoroCycle)
+  renderProgressbar(intervalTimes.workInterval, intervalTimes.breakInterval, intervalTimes.lastBreakInterval, intervalTimes.pomodoroCycle)
 }
 
 const getIntervalTime = () => {
@@ -10,14 +10,14 @@ const getIntervalTime = () => {
         resolve({
           workInterval: result.customizedInterval.workInterval,
           breakInterval: result.customizedInterval.breakInterval,
-          lastBreak: result.customizedInterval.lastBreak,
+          lastBreakInterval: result.customizedInterval.lastBreakInterval,
           pomodoroCycle: result.customizedInterval.pomodoroCycle
         })
       } else {
         resolve({
           workInterval: 25,
           breakInterval: 5,
-          lastBreak: 15,
+          lastBreakInterval: 15,
           pomodoroCycle: 4
         })
       }
@@ -25,18 +25,18 @@ const getIntervalTime = () => {
   })
 }
 
-const renderProgressbar = (workInterval, breakInterval, lastBreak, pomodoroCycle) => {
+const renderProgressbar = (workInterval, breakInterval, lastBreakInterval, pomodoroCycle) => {
   const elementPomodoroProgress = document.querySelector('[data-role="pomodoroProgress"]')
 
-  const percantageIntervalWidths = calculateIntervalWidths(workInterval, breakInterval, lastBreak, pomodoroCycle)
+  const percantageIntervalWidths = calculateIntervalWidths(workInterval, breakInterval, lastBreakInterval, pomodoroCycle)
   buildProgressbar(elementPomodoroProgress, percantageIntervalWidths, pomodoroCycle)
 }
 
-const calculateIntervalWidths = (workInterval, breakInterval, lastBreak, pomodoroCycle) => {
-  const totalMinutes = (workInterval * pomodoroCycle) + (breakInterval * (pomodoroCycle - 1) + lastBreak)
+const calculateIntervalWidths = (workInterval, breakInterval, lastBreakInterval, pomodoroCycle) => {
+  const totalMinutes = (workInterval * pomodoroCycle) + (breakInterval * (pomodoroCycle - 1) + lastBreakInterval)
   const widthWorkInterval = calculateIntervalWidth(workInterval, totalMinutes).toFixed(2)
   const widthBreakInterval = calculateIntervalWidth(breakInterval, totalMinutes).toFixed(2)
-  const widthLastBreakInterval = calculateIntervalWidth(lastBreak, totalMinutes).toFixed(2)
+  const widthLastBreakInterval = calculateIntervalWidth(lastBreakInterval, totalMinutes).toFixed(2)
 
   return [widthWorkInterval, widthBreakInterval, widthLastBreakInterval]
 }
@@ -46,21 +46,21 @@ const calculateIntervalWidth = (interval, totalMinutes) => {
 }
 
 const buildProgressbar = (elementPomodoroProgress, intervalWidthsPercent, pomodoroCycle) => {
-  const workProgressbar = (id, width) => `
+  const workInterval = (id, width) => `
     <div class="work-progressbar" style="width: ${width}%">
-      <div class="work-progressbar__progress" data-role="workProgressbar" data-id="${id}"></div>
+      <div class="work-progressbar__progress" data-role="workInterval" data-id="${id}"></div>
     </div>
   `
 
-  const breakProgressbar = (id, width) => `
+  const breakInterval = (id, width) => `
     <div class="break-progressbar" style="width: ${width}%">
-      <div class="break-progressbar__progress" data-role="breakProgressbar" data-id="${id}"></div>
+      <div class="break-progressbar__progress" data-role="breakInterval" data-id="${id}"></div>
     </div>
   `
 
-  const lastBreakProgressbar = (id, width) => `
+  const lastBreakInterval = (id, width) => `
     <div class="break-progressbar break-progressbar--last-break" style="width: ${width}%">
-      <div class="break-progressbar__progress" data-role="lastBreakProgressbar" data-id="${id}"></div>
+      <div class="break-progressbar__progress" data-role="lastBreakInterval" data-id="${id}"></div>
     </div>
   `
 
@@ -68,9 +68,9 @@ const buildProgressbar = (elementPomodoroProgress, intervalWidthsPercent, pomodo
 
   for (let i = 0; i < pomodoroCycle * 2; i++) {
     if (i === pomodoroCycle * 2 - 1) {
-      progressBar += lastBreakProgressbar(i, intervalWidthsPercent[2])
+      progressBar += lastBreakInterval(i, intervalWidthsPercent[2])
     } else {
-      progressBar += i % 2 === 0 ? workProgressbar(i, intervalWidthsPercent[0]) : breakProgressbar(i, intervalWidthsPercent[1])
+      progressBar += i % 2 === 0 ? workInterval(i, intervalWidthsPercent[0]) : breakInterval(i, intervalWidthsPercent[1])
     }
   }
 
